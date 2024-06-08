34f968ea2f04678ff58ed44cf34335771264bbd9
Kate <ezakimatova@gmail.com>
2024-06-08 12:22:53 +0200

    constants and types for altered scales

--- src/common/scales.ts
+++ src/common/scales.ts
@@ -1,6 +1,48 @@
-import { Scale, ScaleSchema } from './types.ts'
+import {
+    Degree,
+    IAlteredScale,
+    AlteredScale,
+    Scale,
+    ScaleSchema,
+} from './types.ts'
 
 export const scaleSchema: Record<Scale, ScaleSchema> = {
     [Scale.Major]: [2, 2, 1, 2, 2, 2, 1],
     [Scale.NaturalMinor]: [2, 1, 2, 2, 1, 2, 2],
+}
+
+export const alteredScales: Record<AlteredScale, IAlteredScale> = {
+    [AlteredScale.HarmonicMinor]: {
+        name: AlteredScale.HarmonicMinor,
+        base: Scale.NaturalMinor,
+        alterations: [{ degree: Degree.VII, action: 'sharpen' }],
+    },
+    [AlteredScale.Locrian]: {
+        name: AlteredScale.Locrian,
+        base: Scale.NaturalMinor,
+        alterations: [
+            { degree: Degree.II, action: 'flatten' },
+            { degree: Degree.V, action: 'flatten' },
+        ],
+    },
+    [AlteredScale.Dorian]: {
+        name: AlteredScale.Dorian,
+        base: Scale.NaturalMinor,
+        alterations: [{ degree: Degree.VI, action: 'sharpen' }],
+    },
+    [AlteredScale.Phrygian]: {
+        name: AlteredScale.Phrygian,
+        base: Scale.NaturalMinor,
+        alterations: [{ degree: Degree.II, action: 'flatten' }],
+    },
+    [AlteredScale.Lydian]: {
+        name: AlteredScale.Lydian,
+        base: Scale.Major,
+        alterations: [{ degree: Degree.IV, action: 'sharpen' }],
+    },
+    [AlteredScale.Mixolydian]: {
+        name: AlteredScale.Mixolydian,
+        base: Scale.Major,
+        alterations: [{ degree: Degree.V, action: 'flatten' }],
+    },
 }
--- src/common/types.ts
+++ src/common/types.ts
@@ -43,11 +43,31 @@ export type ScaleNotes = [Note, Note, Note, Note, Note, Note, Note]
 export enum Scale {
     Major = 'Major',
     NaturalMinor = 'Minor',
-    // HarmonicMinor = 'harmonic minor',
-    // Phrygian = 'phrygian',
-    // Locrian = 'locrian',
-    // Lydian = 'lydian',
-    // Dorian = 'dorian',
+}
+
+export enum AlteredScale {
+    HarmonicMinor = 'harmonic minor',
+    Phrygian = 'phrygian',
+    Locrian = 'locrian',
+    Lydian = 'lydian',
+    Dorian = 'dorian',
+    Mixolydian = 'mixolydian mode',
+}
+
+export enum Degree {
+    I,
+    II,
+    III,
+    IV,
+    V,
+    VI,
+    VII,
+}
+
+export interface IAlteredScale {
+    name: AlteredScale
+    base: Scale
+    alterations: { degree: Degree; action: 'sharpen' | 'flatten' }[]
 }
 
 export type Color = 'green' | 'blue' | 'orange' | 'yellow' | 'pink' | 'violet'
