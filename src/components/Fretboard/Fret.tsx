b980f09a1765228bf1467ed88a811ca2792037f7
Kate <ezakimatova@gmail.com>
2024-10-12 22:11:11 +0300

    show correct sign

--- src/components/Fretboard/Fret.tsx
+++ src/components/Fretboard/Fret.tsx
@@ -1,8 +1,9 @@
 import { Badge, Text } from '@radix-ui/themes'
-import { Note, noteName } from '../../common/types.ts'
+import { Note } from '../../common/types.ts'
 import { Cell } from './Grid/Cell.tsx'
 import { useSettings } from '../Settings/useSettings.ts'
 import React from 'react'
+import { getNoteName } from '../../common/utils.ts'
 
 interface FretProps {
     note: Note
@@ -10,19 +11,20 @@ interface FretProps {
 
 export const Fret = React.memo(({ note }: FretProps) => {
     const {
-        scale: { notesSet },
+        scale: { notesSet, preferredNaming },
         color,
         showAllNotes,
     } = useSettings()
     const noteColor = notesSet.has(note) ? color.default : undefined
+    const noteName = getNoteName(note, preferredNaming)
     return (
         <Cell>
             {noteColor ? (
                 <Badge size="3" color={noteColor || 'gray'}>
-                    {noteName[note]}
+                    {noteName}
                 </Badge>
             ) : (
-                showAllNotes && <Text size="1">{noteName[note]}</Text>
+                showAllNotes && <Text size="1">{noteName}</Text>
             )}
         </Cell>
     )
